1897b8001265c53f6d8685d2df276b6a4b812abb
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2021-06-14 22:09:20 -0300

    checkpoint
    biome plains
    to do:
    select material

--- Assets/AKCondinoO/VoxelTerrain/World.cs
+++ Assets/AKCondinoO/VoxelTerrain/World.cs
@@ -1,5 +1,6 @@
 using LibNoise;
 using LibNoise.Generator;
+using LibNoise.Operator;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -169,7 +170,17 @@ Modules.Add(new Const(128));
 if(LOG&&LOG_LEVEL<=1)Debug.Log("SetModules() at "+GetType()+" resulted in Count:"+Modules.Count);
 }
 #endregion 
-protected virtual double smoothHeight(double sharpValue){double value=sharpValue;
+protected virtual double smoothDensity(double sharpValue,Vector3 noiseInput,double noiseValue1,float smoothingDelta=5f){double value=sharpValue;
+
+//...
+double delta=(noiseValue1-noiseInput.y);//  noiseInput.y sempre será menor ou igual a noiseValue1
+if(delta<=smoothingDelta){
+double smoothingValue=(smoothingDelta-delta)/smoothingDelta;
+//...
+value*=1d-smoothingValue;
+//...
+
+}
 
 //...
 
@@ -181,16 +192,52 @@ double noiseValue1=noiseCache1[noiseCache1Index]!=0?noiseCache1[noiseCache1Index
 if(noiseInput.y<=noiseValue1){
 
 //...
+v=new TerrainChunk.Voxel(smoothDensity(100,noiseInput,noiseValue1),Vector3.zero,TerrainChunk.MaterialId.Dirt);return;
 
 }
 
 //...
-                  if(vCoord2.y<=128){v=new TerrainChunk.Voxel(100,Vector3.zero,TerrainChunk.MaterialId.Dirt);return;}
-    if(vCoord2.z>=1&&vCoord2.z<=4&&vCoord2.y<=132){v=new TerrainChunk.Voxel(100,Vector3.zero,TerrainChunk.MaterialId.Rock);return;}
+    //              if(vCoord2.y<=128){v=new TerrainChunk.Voxel(100,Vector3.zero,TerrainChunk.MaterialId.Dirt);return;}
+    //if(vCoord2.z>=1&&vCoord2.z<=4&&vCoord2.y<=132){v=new TerrainChunk.Voxel(100,Vector3.zero,TerrainChunk.MaterialId.Rock);return;}
 
 v=TerrainChunk.Voxel.Air;}
 }
 public class Plains:BiomeBase{
+public override int IdxForRnd{get{return 1;}}
+public override int IdxForHgt{get{return 5;}}//  Base Height Result Module
+protected override void SetModules(){
+                   base.SetModules();
+#region 1
+ModuleBase module1=new Const(5);
+#endregion
+#region 2
+ModuleBase module2a=new RidgedMultifractal(frequency:Mathf.Pow(2,-8),lacunarity:2.0,octaves:6,seed:Random[IdxForRnd].Next(),quality:QualityMode.Low);
+ModuleBase module2b=new Turbulence(input:module2a); 
+((Turbulence)module2b).Seed=Random[IdxForRnd].Next();
+((Turbulence)module2b).Frequency=Mathf.Pow(2,-2);
+((Turbulence)module2b).Power=1;
+ModuleBase module2c=new ScaleBias(scale:1.0,bias:30.0,input:module2b);  
+#endregion
+#region 3
+ModuleBase module3a=new Billow(frequency:Mathf.Pow(2,-7)*1.6,lacunarity:2.0,persistence:0.5,octaves:8,seed:Random[IdxForRnd].Next(),quality:QualityMode.Low);
+ModuleBase module3b=new Turbulence(input:module3a);
+((Turbulence)module3b).Seed=Random[IdxForRnd].Next();
+((Turbulence)module3b).Frequency=Mathf.Pow(2,-2);  
+((Turbulence)module3b).Power=1.8;
+ModuleBase module3c=new ScaleBias(scale:1.0,bias:31.0,input:module3b);
+#endregion
+#region 4
+ModuleBase module4a=new Perlin(frequency:Mathf.Pow(2,-6),lacunarity:2.0,persistence:0.5,octaves:6,seed:Random[IdxForRnd].Next(),quality:QualityMode.Low);
+ModuleBase module4b=new Select(inputA:module2c,inputB:module3c,controller:module4a);
+((Select)module4b).SetBounds(min:-.2,max:.2);
+((Select)module4b).FallOff=.25;
+ModuleBase module4c=new Multiply(lhs:module4b,rhs:module1);
+#endregion
+Modules.Add(module4c);
+
+//...
+
+}
 }
 }
 }
