13e6e53f27dca067e1442a20539192563badc14a
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2021-07-08 07:39:44 -0300

    Update Buildings.cs

--- Assets/Resources/AKCondinoO/Buildings/Buildings.cs
+++ Assets/Resources/AKCondinoO/Buildings/Buildings.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-namespace AKCondinoO.Buildings{public class Buildings:MonoBehaviour{
+using static AKCondinoO.Voxels.TerrainChunk;using static AKCondinoO.Voxels.World;
+namespace AKCondinoO.Buildings{public class Buildings:MonoBehaviour{public bool LOG=true;public int LOG_LEVEL=1;
+[NonSerialized]public static string buildingsPath;[NonSerialized]public static string buildingsFolder;
+[NonSerialized]public static readonly List<object>load_Syn_All=new List<object>();
+[NonSerialized]static Dictionary<Type,int>Count;[NonSerialized]static Dictionary<Type,List<int>>Destroyed;
 [NonSerialized]public static Buildings staticScript;
 void Awake(){staticScript=this;
+buildingsFolder=string.Format("{0}{1}",savePath,"buildings");
+Directory.CreateDirectory(buildingsPath=string.Format("{0}/",buildingsFolder));
 
             
 
