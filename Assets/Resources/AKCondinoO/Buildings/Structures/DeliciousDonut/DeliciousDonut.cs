32df763622bb155fd7b830b2c117a5c3eed00150
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2021-07-28 12:12:13 -0300

    ongoing do navMesh

--- Assets/Resources/AKCondinoO/Buildings/Structures/DeliciousDonut/DeliciousDonut.cs
+++ Assets/Resources/AKCondinoO/Buildings/Structures/DeliciousDonut/DeliciousDonut.cs
@@ -1,6 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static AKCondinoO.Util;using static AKCondinoO.Voxels.TerrainChunk;using static AKCondinoO.Voxels.World;using static AKCondinoO.Buildings.Buildings;
 namespace AKCondinoO.Buildings{public class DeliciousDonut:Furniture{
+[SerializeField]MeshFilter meshFilter;[NonSerialized]Mesh mesh;
+[NonSerialized]NavMeshBuildSource navMeshSource;
+protected override void Awake(){
+                   base.Awake();
+
+//...
+
+mesh=meshFilter.mesh;
+navMeshSource=new NavMeshBuildSource{
+transform=transform.localToWorldMatrix,
+shape=NavMeshBuildSourceShape.Mesh,
+sourceObject=mesh,
+component=meshFilter,
+area=1,//  not walkable
+};
+
+//...
+
+}
 }
 }
