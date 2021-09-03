5cdb5c23f65b405db11be54fc331d95580e89ee4
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2021-09-03 19:30:27 -0300

    Update Cnidoscolus_quercifolius.cs
    to do
    fazer pool de plantas
    limpar código de debug.logwarning

--- Assets/Resources/AKCondinoO/Buildings/Structures/Biomes/Plains/Cnidoscolus_quercifolius/Cnidoscolus_quercifolius.cs
+++ Assets/Resources/AKCondinoO/Buildings/Structures/Biomes/Plains/Cnidoscolus_quercifolius/Cnidoscolus_quercifolius.cs
@@ -6,7 +6,7 @@ using System.Collections.ObjectModel;
 using UnityEngine;
 namespace AKCondinoO.Buildings.Biomes.Plains{public class Cnidoscolus_quercifolius:Plant{
 [NonSerialized]public static readonly ReadOnlyCollection<(Type type,float chance,Vector3 minScale,Vector3 maxScale)>Biomes=new ReadOnlyCollection<(Type,float,Vector3,Vector3)>(new (Type,float,Vector3,Vector3)[1]{
-(typeof(World.Plains),.5f,Vector3.one*.1f,Vector3.one*.5f),
+(typeof(World.Plains),.3f,Vector3.one*.1f,Vector3.one*.5f),
 });[NonSerialized]public const int maxDepth=1;
 [NonSerialized]public const float radius=3.75f;
 [NonSerialized]public const float spacingMultiplier=1f;
